77caf3fa6a2344ef6b9fa89acf9390bceeb95fb5
Puzaan Xhres <41267289+puzaan@users.noreply.github.com>
2021-05-12 11:16:16 +0545

    setting env

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const products = require("./data/products");
 const cors = require("cors");
+const { request } = require("express");
+const dotencv = request('dotenv');
+dotencv.config();
 const app = express();
 
 
-app.use(cors());
 
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("hello world");
@@ -19,6 +23,6 @@ app.get("/api/products/:id", (req, res) => {
   const product = products.find((p) => p._id === req.params.id);
   res.json(product);
 });
+const PORT = process.env.PORT || 5000;
 
-
-app.listen(5000, console.log("server is running on 500"));
+app.listen(PORT, console.log(`server is running on ${PORT}`));
